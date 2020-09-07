0b2beb836641d2a60c112e89baf5b0d1074fb58c
A-Ignacio <a.ignacio.dev@gmail.com>
2020-09-07 15:02:54 -0300

    Primera correción, se agregaron la suma, otro archivo html y ccreo que hay algo que se coló

--- js/scriptSuma.js
+++ js/scriptSuma.js
@@ -1,3 +1,7 @@
-function suma() {
-
-}
+function suma() 
+        {
+            var n1 = parseInt(document.getElementById("number1").value);
+            var n2 = parseInt(document.getElementById("number2").value);
+            var res = n1 + n2;
+            document.getElementById("res").value=res;
+    }    
