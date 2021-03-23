91c282a55a666424015e9c94c5faa0a255854a8d
Víctor Díaz Cortés <vmdcortes@gmail.com>
2021-03-22 22:01:52 -0300

    Update n1, Piratas Dulces

--- js/scriptMulti.js
+++
@@ -1,7 +0,0 @@
-function Multi() 
-        {
-            var n1 = parseInt(document.getElementById("number1").value);
-            var n2 = parseInt(document.getElementById("number2").value);
-            var res = n1 * n2;
-            document.getElementById("res").value=res;
-    }    
